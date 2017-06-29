bbeb48184ca26dae3c966a983695779fda411ae5
Michael Bandola <24bandola@gmail.com>
2017-06-29 18:42:16 -0400

    commit

--- Assets/__Scripts/Prospector.cs
+++ Assets/__Scripts/Prospector.cs
@@ -9,14 +9,31 @@ public class Prospector : MonoBehaviour {
 	public Deck					deck;
 	public TextAsset			deckXML;
 
+	public Layout layout;
+	public TextAsset layoutXML;
+
 	void Awake(){
 		S = this;
 	}
+
+	public List<CardProspector> drawPile;
 
 	void Start() {
 		deck = GetComponent<Deck> ();
 		deck.InitDeck (deckXML.text);
 		Deck.Shuffle (ref deck.cards);
+		layout = GetComponent<Layout> ();
+		layout.ReadLayout (layoutXML.text);
+		drawPile = ConvertListCardsToListCardProspectors (deck.cards);
 	}
 
+	List<CardProspector> ConvertListCardsToListCardProspectors(List<Card>lCD){
+		List<CardProspector> lCP = new List<CardProspector> ();
+		CardProspector tCP;
+		foreach (Card tCD in lCD) {
+			tCP = tCD as CardProspector;
+			lCP.Add (tCP);
+		}
+		return (lCP);
+	}
 }
